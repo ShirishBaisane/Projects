ca2b2a2d6ff8eb93d0d9f4f539d7cb95e88f5d85
Shirish Baisane <shirish.baisane@gmail.com>
2023-05-02 21:18:11 +0530

    Online Coffee Shop: Responsive design completed.

--- Coffee Shop/js/main.js
+++ Coffee Shop/js/main.js
@@ -1,12 +1,27 @@
+// For Search Icon
 let search = document.querySelector('.search-box');
 console.log(search);
 console.log(document.querySelector('#search-icon'));
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
+// For Menu Icon
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+// Remove Menu and Search window whem scrolled
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
 
 let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
